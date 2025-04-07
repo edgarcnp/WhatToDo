ceac36066476b7e9589be881b2a725b0a2c978de
Edgar Christian <edgarcnp@proton.me>
2025-04-08 02:47:54 +0700

    fix(backend): hardcoded URL (#3)
    * fix(backend): hardcoded URL
    
    * minor fix
    
    * unused import
    
    * package thingy

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,27 +1,27 @@
-#![allow(unused)] // Remove later
 use std::sync::Arc;
 
 use axum::serve;
 use axum_error::Result;
-use sqlx::{pool, sqlite::SqlitePool};
+use sqlx::sqlite::SqlitePool;
 use tokio::net::TcpListener;
 
 mod routes;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let _ = dotenv::dotenv(); // Get environment variables
+    let _ = dotenv::dotenv(); // Load .env
     let url = std::env::var("DATABASE_URL")?;
+    let address = std::env::var("SERVER_ADDRESS")?;
 
-    // Database connection
+    // DB connection
     let pool = SqlitePool::connect(&url).await?;
     let shared_pool = Arc::new(pool);
 
-    // Define routes
+    // Axum routes
     let router = routes::app_routes(shared_pool);
 
-    // Start the server
-    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    println!("Listening on http://0.0.0.0:3000");
-    Ok(axum::serve(listener, router).await.unwrap())
+    // Bind to address
+    let listener = TcpListener::bind(&address).await.unwrap();
+    println!("Listening on: {}", &address);
+    Ok(serve(listener, router).await.unwrap())
 }
